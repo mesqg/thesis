ee30ee723cbf5373fc0ebe07888ffbe6ab7724d5
mesqg <45458388+mesqg@users.noreply.github.com>
2019-03-27 11:47:15 +0100

    Add files via upload

--- KHC-Francisco/Tests/test3.hs
+++ KHC-Francisco/Tests/test3.hs
@@ -6,10 +6,11 @@ data Bool = B
 data Chuck  = C
 data Dreamz = D
 --comment yoley out to see it working
-implicit yoley : Bool Arm = \x. D --Use to see that the 
+--implicit yoley : Bool Arm = \x. D --Use to see that the 
 implicit yupa : Dreamz Chuck =\x. C
 implicit yamano : Arm Bool = \x. B
 implicit yeye : Bool Chuck = \x. C
+implicit f : Dreamz Arm = \x. A  
 
 class Eq a :: * where
   equals :: a -> a -> Bool
@@ -17,13 +18,18 @@ class Eq a :: * where
 class Eq a => Ord a :: * where
   compare :: a -> a -> Bool
 
-instance Eq Bool where
-  equals = \x. \y. B
+-- Should Find Eq Chuck but doesnt atm. If we uncomment next lines all works because we got luck.
+--instance Eq Bool where
+--  equals = \x. \y. B
+
+instance Eq Chuck where
+  equals = \x. \y. B    
 
 -- | Program expression
 
 --(equals B A) --all good
 --(equals C A)
---(equals A B) -- nop
-(equals C B)
+--(equals A D) -- nop
+--(equals C B)
 --(equals Yeah AA) --no backtracking
+(\x. equals A x)(equals D B)
