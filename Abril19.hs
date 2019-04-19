fd9e4b603b46433afa8d701cc81e7817c9956812
fmg <francisco.gabriel@student.kuleuven.be>
2019-04-19 12:40:48 +0200

    It had some dumb mistakes

--- Abril19.hs
+++ Abril19.hs
@@ -1,3 +1,11 @@
+-- * NEW CODE FOR SOLVING IMPLICIT CONSTRAINTS
+-- | Completely entail the set of Implicit constraints. This allows to try several orders in which to solve the set.
+solveY :: YCs -> ImplicitTheory -> TcM (FcTmSubst,HsTySubst)
+solveY ycs it = entailYcs (f ycs it) it
+  `catchError` (\s-> throwError (s++" :solveY"))
+   
+  
+-- TODO duplicated code! -}
 -- | Completely entail a set of conversion constraints. Fail if not possible
 entailYcs :: YCs -> ImplicitTheory -> TcM (FcTmSubst,HsTySubst)
 entailYcs SN it = return mempty
@@ -53,26 +61,27 @@ reachable :: RnMonoTy -> ImplicitTheory -> [RnMonoTy]
 reachable init it = aux [init] [] it
    where 
          aux :: [RnMonoTy]->[RnMonoTy]->ImplicitTheory->[RnMonoTy]
-         aux acc q@(at:rest) (xs:>x) = case onestep acc at x of 
+         aux acc q@(at:rest) (xs:>x) = case onestep (acc `union` q)  at x of 
              Just v -> aux acc (q++[v]) xs
              Nothing -> aux acc q xs
          aux acc (at:rest) SN = aux (at:acc) rest it
          aux acc [] _ = acc
 
          onestep :: [RnMonoTy]->RnMonoTy->ConvAxiom->Maybe RnMonoTy
-         onestep acc at (UC (MCT a b) exp)
+         onestep old at (UC (MCT a b) exp)
           | Right ty_subst <- unify [] [at :~: a] =
-              if (substInMonoTy ty_subst b) `elem` acc then Nothing else
+              if (substInMonoTy ty_subst b) `elem` old then Nothing else
                 Just (substInMonoTy ty_subst b)
           | Left _         <- unify [] [at :~: a] = Nothing
           
 -- | propagates type to vars with only one incomming edge
 step1 :: YCs -> YCs
 step1 all@(rest:>conv@(YCt _ (MCT a (TyVar b))))
- | one_incomming (TyVar b) all = substInYCs ( b |-> a) rest
+ | one_incomming (TyVar b) all 0 = (step1 (substInYCs ( b |-> a) rest)):>(substInYCt ( b |-> a) conv)
  | otherwise      = (step1 rest):>conv
- where one_incomming b SN = True
-       one_incomming b (xs:>x@(YCt _ (MCT _ c))) = if c == b then False else one_incomming b xs
+ where one_incomming b SN 1 = True
+       one_incomming b SN _ = False
+       one_incomming b (xs:>x@(YCt _ (MCT _ c))) i = if c == b then one_incomming b xs (i+1) else one_incomming b xs i
 step1 SN = SN
 
 -- | look for a common type where they can meet
@@ -83,7 +92,8 @@ step2 ycs it = aux2 ycs it
   aux2 (rest:>conv@(YCt _ (MCT a b))) it = case b of
     TyVar x -> let new = (foldr intersect (reachable (head (incomming b ycs)) it) (map (\x->reachable x it) (tail (incomming b ycs))))!!0 in
      let subst = x |-> new in
-       step2 (substInYCs subst ycs) it
+       --step2 (substInYCs subst ycs) it
+       (step2 (substInYCs subst rest) it):>(substInYCt subst conv)
     otherwise -> (aux2 rest it):>conv
 
   incomming :: RnMonoTy -> YCs -> [RnMonoTy]
